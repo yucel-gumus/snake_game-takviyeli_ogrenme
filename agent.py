1309a57f99b01a7eb67b7aad80a46fbb95e05f9d
Yücel Gümüş <yucelgumus61@yahoo.com>
2024-01-16 20:41:38 +0300

    update_model

---
+++ agent.py
@@ -0,0 +1,147 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
+import os
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.007
+NORMALIZATION_FACTOR = 20  # Normalizasyon için faktör
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0
+        self.gamma = 0.9
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    def get_state(self, game):
+        head = game.snake[0]
+        point_l = Point(head.x - NORMALIZATION_FACTOR, head.y)
+        point_r = Point(head.x + NORMALIZATION_FACTOR, head.y)
+        point_u = Point(head.x, head.y - NORMALIZATION_FACTOR)
+        point_d = Point(head.x, head.y + NORMALIZATION_FACTOR)
+
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
+
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+        ]
+
+        return np.array(state, dtype=int)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0, 0]
+
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
+    def normalize_data(self, data):
+        return data / NORMALIZATION_FACTOR  # Veriyi normalize etmek için bir metot
+
+    def save_model(self):
+        try:
+            os.makedirs('./model')
+        except OSError:
+            pass
+        torch.save(self.model.state_dict(), './model/model.pth')
+
+    def load_model(self):
+        if os.path.exists('./model/model.pth'):
+            self.model.load_state_dict(torch.load('./model/model.pth'))
+            print("Mevcut model yüklendi.")
+        else:
+            print("Mevcut model bulunamadı.")
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    agent.load_model()  # Modeli yükle
+    game = SnakeGameAI()
+    
+    while True:
+        state_old = agent.get_state(game)
+        final_move = agent.get_action(state_old)
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.save_model()  # Modeli kaydet
+
+            print('Oyun', agent.n_games, 'Skor', score, 'Rekor:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+if __name__ == '__main__':
+    train()
